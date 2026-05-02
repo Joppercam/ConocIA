c70bfaa25bda26e48824a48160e1790fb26e87a0
Juan pablo Basualdo <juanpablobasualdo@MacBook-Pro-de-Juan.local>
2026-05-02 19:12:45 -0400

    Configura IDs de Google Analytics

--- config/services.php
+++ config/services.php
@@ -114,7 +114,8 @@ return [
 
     'analytics' => [
         'enabled' => env('ANALYTICS_ENABLED', true),
-        'ga4_measurement_id' => env('GOOGLE_ANALYTICS_MEASUREMENT_ID'),
+        'ga4_measurement_id' => env('GOOGLE_ANALYTICS_MEASUREMENT_ID', 'G-W2M9Q5P8MQ'),
+        'google_tag_id' => env('GOOGLE_TAG_ID', 'GT-TNP9X5ZP'),
         'gtm_container_id' => env('GOOGLE_TAG_MANAGER_ID'),
     ],
 
--- resources/views/partials/analytics-head.blade.php
+++ resources/views/partials/analytics-head.blade.php
@@ -1,7 +1,9 @@
 @php
     $analyticsEnabled = (bool) config('services.analytics.enabled');
     $ga4MeasurementId = trim((string) config('services.analytics.ga4_measurement_id'));
+    $googleTagId = trim((string) config('services.analytics.google_tag_id'));
     $gtmContainerId = trim((string) config('services.analytics.gtm_container_id'));
+    $gtagLoaderId = $googleTagId !== '' ? $googleTagId : $ga4MeasurementId;
 @endphp
 
 @if($analyticsEnabled && $gtmContainerId !== '')
@@ -16,14 +18,17 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <!-- End Google Tag Manager -->
 @endif
 
-@if($analyticsEnabled && $ga4MeasurementId !== '')
-<!-- Google Analytics 4 -->
-<script async src="https://www.googletagmanager.com/gtag/js?id={{ $ga4MeasurementId }}"></script>
+@if($analyticsEnabled && $gtagLoaderId !== '')
+<!-- Google tag / Google Analytics 4 -->
+<script async src="https://www.googletagmanager.com/gtag/js?id={{ $gtagLoaderId }}"></script>
 <script>
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
+gtag('config', '{{ $gtagLoaderId }}');
+@if($ga4MeasurementId !== '' && $ga4MeasurementId !== $gtagLoaderId)
 gtag('config', '{{ $ga4MeasurementId }}');
+@endif
 </script>
-<!-- End Google Analytics 4 -->
+<!-- End Google tag / Google Analytics 4 -->
 @endif
